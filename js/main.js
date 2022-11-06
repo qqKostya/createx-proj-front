e4d5614696c638545163cd9e5c06fabd3beb711d
qqKostya <leap74rus@gmail.com>
2022-11-06 18:30:10 +0300

    add: section program and accordion

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-$(function() {
+$(function () {
   var mixer = mixitup('.directions__list');
 
-  $('.directions__filter-btn').on('click', function() {
+  $('.directions__filter-btn').on('click', function () {
     $('.directions__filter-btn').removeClass('directions__filter-btn--active')
     $(this).addClass('directions__filter-btn--active');
   })
@@ -13,11 +13,11 @@ $(function() {
     draggable: false,
     waitForAnimate: false,
   })
-  $('.team__slider-prev').on('click',function(e) {
+  $('.team__slider-prev').on('click', function (e) {
     e.preventDefault()
     $('.team__slider').slick('slickPrev')
   })
-  $('.team__slider-next').on('click',function(e) {
+  $('.team__slider-next').on('click', function (e) {
     e.preventDefault()
     $('.team__slider').slick('slickNext')
   })
@@ -28,12 +28,31 @@ $(function() {
     appendDots: $('.testimonials__dots'),
     waitForAnimate: false,
   })
-  $('.testimonials__prev').on('click',function(e) {
+  $('.testimonials__prev').on('click', function (e) {
     e.preventDefault()
     $('.testimonials__slider').slick('slickPrev')
   })
-  $('.testimonials__next').on('click',function(e) {
+  $('.testimonials__next').on('click', function (e) {
     e.preventDefault()
     $('.testimonials__slider').slick('slickNext')
+  })
+
+  // $('.program__acc-link').on('click', function (e) {
+  //   e.preventDefault();
+  //   $(this).toggleClass('program__acc-link--active')
+  //   $(this).children('.program__acc-text').slideToggle()
+  // })
+
+  $('.program__acc-link').on('click', function (e) {
+    e.preventDefault()
+    if ($(this).hasClass('program__acc-link--active')) {
+      $(this).removeClass('program__acc-link--active')
+      $(this).children('.program__acc-text').slideUp()
+    } else {
+      $('.program__acc-link').removeClass('program__acc-link--active')
+      $('.program__acc-text').slideUp()
+      $(this).addClass('program__acc-link--active')
+      $(this).children('.program__acc-text').slideDown()
+    }
   })
 })
