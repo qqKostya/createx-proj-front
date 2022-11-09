4e84e4f846f24fe54853852dde74e45c25ebe1e8
qqKostya <leap74rus@gmail.com>
2022-11-09 21:51:39 +0300

    finish desktop version

--- js/main.js
+++ js/main.js
@@ -1,41 +1,41 @@
 $(function () {
-  var mixer = mixitup('.directions__list');
+  var mixer = mixitup(".directions__list");
 
-  $('.directions__filter-btn').on('click', function () {
-    $('.directions__filter-btn').removeClass('directions__filter-btn--active')
-    $(this).addClass('directions__filter-btn--active');
-  })
+  $(".directions__filter-btn").on("click", function () {
+    $(".directions__filter-btn").removeClass("directions__filter-btn--active");
+    $(this).addClass("directions__filter-btn--active");
+  });
 
-  $('.team__slider').slick({
+  $(".team__slider").slick({
     arrows: false,
     slidesToShow: 4,
     infinity: true,
     draggable: false,
     waitForAnimate: false,
-  })
-  $('.team__slider-prev').on('click', function (e) {
-    e.preventDefault()
-    $('.team__slider').slick('slickPrev')
-  })
-  $('.team__slider-next').on('click', function (e) {
-    e.preventDefault()
-    $('.team__slider').slick('slickNext')
-  })
+  });
+  $(".team__slider-prev").on("click", function (e) {
+    e.preventDefault();
+    $(".team__slider").slick("slickPrev");
+  });
+  $(".team__slider-next").on("click", function (e) {
+    e.preventDefault();
+    $(".team__slider").slick("slickNext");
+  });
 
-  $('.testimonials__slider').slick({
+  $(".testimonials__slider").slick({
     arrows: false,
     dots: true,
-    appendDots: $('.testimonials__dots'),
+    appendDots: $(".testimonials__dots"),
     waitForAnimate: false,
-  })
-  $('.testimonials__prev').on('click', function (e) {
-    e.preventDefault()
-    $('.testimonials__slider').slick('slickPrev')
-  })
-  $('.testimonials__next').on('click', function (e) {
-    e.preventDefault()
-    $('.testimonials__slider').slick('slickNext')
-  })
+  });
+  $(".testimonials__prev").on("click", function (e) {
+    e.preventDefault();
+    $(".testimonials__slider").slick("slickPrev");
+  });
+  $(".testimonials__next").on("click", function (e) {
+    e.preventDefault();
+    $(".testimonials__slider").slick("slickNext");
+  });
 
   // $('.program__acc-link').on('click', function (e) {
   //   e.preventDefault();
@@ -43,16 +43,33 @@ $(function () {
   //   $(this).children('.program__acc-text').slideToggle()
   // })
 
-  $('.program__acc-link').on('click', function (e) {
-    e.preventDefault()
-    if ($(this).hasClass('program__acc-link--active')) {
-      $(this).removeClass('program__acc-link--active')
-      $(this).children('.program__acc-text').slideUp()
+  $(".program__acc-link").on("click", function (e) {
+    e.preventDefault();
+    if ($(this).hasClass("program__acc-link--active")) {
+      $(this).removeClass("program__acc-link--active");
+      $(this).children(".program__acc-text").slideUp();
     } else {
-      $('.program__acc-link').removeClass('program__acc-link--active')
-      $('.program__acc-text').slideUp()
-      $(this).addClass('program__acc-link--active')
-      $(this).children('.program__acc-text').slideDown()
+      $(".program__acc-link").removeClass("program__acc-link--active");
+      $(".program__acc-text").slideUp();
+      $(this).addClass("program__acc-link--active");
+      $(this).children(".program__acc-text").slideDown();
     }
-  })
-})
+  });
+
+  $(".header__nav-list a, .header__top-btn, .footer__go-top").on("click", function (event) {
+    //отменяем стандартную обработку нажатия по ссылке
+
+    event.preventDefault();
+
+    //забираем идентификатор бока с атрибута href
+
+    var id = $(this).attr("href"),
+      //узнаем высоту от начала страницы до блока на который ссылается якорь
+
+      top = $(id).offset().top - 100;
+
+    //анимируем переход на расстояние - top за 1500 мс
+
+    $("body,html").animate({ scrollTop: top }, 800);
+  });
+});
